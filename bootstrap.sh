04099330194b1bd009f0f37bff0bb513c8ed8d8e
prokopenko <prokopenko@skytracking.ru>
2015-05-17 16:34:32 +0300

    Vagrant file bugfix

--- bootstrap.sh
+++ bootstrap.sh
@@ -1,6 +1,8 @@
 #!/usr/bin/env bash
 
 apt-get update
+debconf-set-selections <<< 'mysql-server mysql-server/root_password password'
+debconf-set-selections <<< 'mysql-server mysql-server/root_password_again password'
 apt-get install -y nginx php5-fpm php5-cli mysql-server
 ln -fs /vagrant/nginx.conf /etc/nginx/sites-enabled/ffsale.loc
 service nginx restart
